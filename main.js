185797dad2728b88a5308045838c4c6564b59c87
Adam Campbell <adamcampbelldev@gmail.com>
2025-01-23 21:26:57 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,133 @@
+import { fetchStaticData, getEmptyCharacter } from "./utils.js";
+
+document.addEventListener("alpine:init", () => {
+    console.log("alpine has initialised");
+    Alpine.prefix("data-")
+    Alpine.data('characterGenerator', () => ({
+        character: null,
+        staticData: null,
+        isLoading: true,
+        currentPage: "race",
+
+        async init() {
+            console.log("cg init ran")
+            try {
+                console.log("cg init ran")
+                this.isLoading = true;
+                this.staticData = await fetchStaticData();
+                this.character = getEmptyCharacter();
+                this.isLoading = false;
+                console.log(this.staticData)
+            } catch (error) {
+                console.error('Error fetching JSON:', error);
+            }
+        },
+        setPage(page) {
+            this.currentPage = page;
+        },
+        /**
+         * Return the static data for the race that has been chosen by the user.
+         */
+        get chosenRace() {
+            return this.staticData.races.find(r => r.id === this.character.race);
+        },
+        /**
+         * Return the static data for the class that has been chosen by the user.
+         */
+        get chosenClass() {
+            return this.staticData.classes.find(c => c.id === this.character.class);
+        },
+        /**
+         * Computes the number of skill proficiencies that the user can choose, based on
+         * their choice of class and race.
+         */
+        get computedNumberOfSkillProficiencies() {
+            const classSkillProficiencies = this.chosenClass ? this.chosenClass.proficiencies.skills.choose : 0;
+            const raceSkillProficiencies = this.chosenRace ? this.chosenRace.additionalSkillProficiencies : 0;
+            return classSkillProficiencies + raceSkillProficiencies;
+        },
+        
+        /**
+         * Adds or removes the given skillId from the classSkillChoices array. Respects the
+         * computedNumberOfSkillProficiencies, removing the oldest skill if the user tries to
+         * add more than the allowed number.
+         * @param {*} skillId 
+         */
+        toggleSkillProficiency(skillId) {
+            const index = this.character.classSkillChoices.indexOf(skillId);
+
+            // If the skill is not already in the array...
+            if (index === -1) {
+                // ... then add it to the array, if there is space.
+                if (this.character.classSkillChoices.length < this.computedNumberOfSkillProficiencies) {
+                    this.character.classSkillChoices.push(skillId);
+                // If there is not space, then remove the first (oldest) skill from the array,
+                // and add the new one at the end
+                } else {
+                    this.character.classSkillChoices.shift();
+                    this.character.classSkillChoices.push(skillId);
+                }
+            // If the skill is already in the array, remove it.
+            } else {
+                this.character.classSkillChoices.splice(index, 1); 
+            }
+        },
+        /**
+         * Return the static data for the ability with the given id
+         * @param {*} abilityId 
+         * @returns 
+         */
+        getAbilityData(abilityId) {
+            return this.staticData.abilities.find(a => a.id === abilityId);
+        },
+        /**
+         * Computes the ability point options for the given abilityId, based on which
+         * points from the standard array have already been chosen.
+         * @param {*} abilityId 
+         * @returns 
+         */
+        computeAbilityPointOptions(abilityId) {
+            // Construct an array containing some subset of the options
+            // '--', 8, 10, 12, 13, 14, 15,
+            // where '--' represents none/unset.
+            // Specifically, it should include None, the current value of this ability,
+            // and any other value that has not been chosen yet.
+            const currentAbilityValue = this.character.abilityPoints.find(a => a.id === abilityId).value;
+            const points = [8, 10, 12, 13, 14, 15].filter(p => {
+                // If the point is the current abilities value, or if there is no ability with that value,
+                // then include it in the list. 
+                return p === currentAbilityValue || !this.character.abilityPoints.find(a => a.value === p);
+            });
+            if (points[0] !== "--") {
+                points.unshift("--");
+            }
+            return points;
+        },
+        /**
+         * Resets the ability points to the default values for the chosen class.
+         */
+        resetAbilityPoints() {
+            const classDefaults = this.chosenClass.abilities;
+            for (let i = 0; i < this.character.abilityPoints.length; i++) {
+                this.character.abilityPoints[i].value = classDefaults[i].value;
+            }
+        },
+        /**
+         * Whenever a different class is chosen, reset the relevant parts of state, either
+         * to empty or to the class default.
+         * @param {*} e 
+         */
+        resetStateOnClassChange(e) {
+            console.log(e.target.value);
+        },
+        /**
+         * When the user has finished creating their character, POST the character to the server.
+         */
+        handleSubmit() {
+            console.log("submitting")
+            const character = JSON.parse(JSON.stringify(this.character));
+            console.log(character)
+        }
+    }));
+})
+
---
+++ utils.js
@@ -0,0 +1,148 @@
+
+
+/**
+ * Factory function for creating a new, empty character object
+ * @returns 
+ */
+export function getEmptyCharacter() {
+    return {
+        race: "",
+        class: "",
+        classSkillChoices: [],
+        classCantripChoices: [],
+        classSpellChoices: [],
+        abilityPoints: [
+            { id: "0caab33e-f424-4a44-94cd-0c6951e5bdfe", value: null },
+            { id: "fd107c7f-4536-4b36-bf43-e49d92a3c4c2", value: null },
+            { id: "b9b14f85-78db-49ea-b07b-b8bdd7a40046", value: null },
+            { id: "44fbcb3c-d548-4a3c-aa85-4c55e05aabed", value: null },
+            { id: "468b3218-340b-4263-9450-dc72e6750f16", value: null },
+            { id: "b15c2aa9-87e7-408d-89a7-3bbd64d981a9", value: null },
+        ],
+        name: "",
+        age: null,
+        gender: "",
+        alignment: "",
+        background: "",
+        traits: [],
+        ideals: [],
+        bonds: [],
+        flaws: [],
+        height: "",
+        build: "",
+        skinTone: "",
+        hairColor: "",
+        hairStyle: "",
+        hairLength: "",
+        hairType: "",
+        facialHairStyle: "",
+        facialHairLength: "",
+        eyeColor: "",
+        eyeShape: "",
+        distinguishingFeatures: [],
+        clothingStyle: "",
+        clothingColors: [],
+        clothingAccessories: [],
+    };
+}
+
+/**
+ * Combines the different items collections into a single items collection,
+ * deletes the original collections.
+ * Mutates the data object rather than creating a new one.
+ * @param {*} data 
+ */
+function combineItems(data) {
+    data.items = [
+        ...data.weapons,
+        ...data.armor,
+        ...data.items
+    ];
+    delete data.weapons;
+    delete data.armor;
+}
+
+/**
+ * Denormalises a single class object, replacing id references with actual entities.
+ * Mutates the class object rather than creating a new one.
+ * @param {*} cls 
+ * @param {*} data 
+ */
+function denormaliseClass(cls, data) {
+    // Equipment needs to have its shape changed
+    cls.equipment = cls.equipment.map(e => {
+        const item = data.items.find(i => i.id === e.id);
+        return {
+            item: item,
+            quantity: e.quantity
+        }
+    });
+    // Abilities need to have their shape changed
+    cls.abilities = cls.abilities.map(abilityValuePair => {
+        const ability = data.abilities.find(i => i.id === abilityValuePair.id);
+        return {
+            ...ability,
+            value: abilityValuePair.value
+        }
+    });
+    // Proficiencies can just be replaced as-is
+    cls.proficiencies.armor = cls.proficiencies.armor.map(a => data.items.find(i => i.id === a));
+    cls.proficiencies.weapons = cls.proficiencies.weapons.map(w => data.items.find(i => i.id === w));
+    cls.proficiencies.savingThrows = cls.proficiencies.savingThrows.map(s => data.abilities.find(i => i.id === s));
+    cls.proficiencies.skills.from = cls.proficiencies.skills.from.map(s => data.skills.find(i => i.id === s));
+    // Spellcasting can be replaced as-is, but only if the class is a caster class.
+    if (cls.spellcasting.ability !== null) {
+        cls.spellcasting.ability = data.abilities.find(a => a.id === cls.spellcasting.ability);
+        cls.spellcasting.cantrips.from = cls.spellcasting.cantrips.from.map(c => data.cantrips.find(s => s.id === c));
+        cls.spellcasting.spells.from = cls.spellcasting.spells.from.map(s => data.spells.find(s => s.id === s));
+    }
+}
+
+/**
+ * Denormalises a single race, replacing id references with actual entities.
+ * Mutates the race object rather than creating a new one.
+ * @param {*} race 
+ * @param {*} data 
+ */
+function denormaliseRace(race, data) {
+    // We need to change the shape of ability bonuses
+    race.abilityBonuses = race.abilityBonuses.map(abilityBonus => {
+        const ability = data.abilities.find(a => a.id === abilityBonus.id);
+        return {
+            ...ability,
+            bonus: abilityBonus.bonus
+        }
+    });
+    // Weapon proficiencies can be replaced as-is
+    race.weaponProficiencies = race.weaponProficiencies.map(w => data.items.find(i => i.id === w));
+}
+
+/**
+ * Denormalises the data. Mutates the data object rather than creating a new one.
+ * a new one.
+ * @param {*} data 
+ * @returns 
+ */
+export function denormaliseData(data) {
+    combineItems(data);
+    data.classes.forEach(cls => denormaliseClass(cls, data));
+    data.races.forEach(race => denormaliseRace(race, data));
+    // return the data object just for flexibility
+    return data;
+}
+
+/**
+ * Fetches the static data JSON file, parses it and denormalises it.
+ * @returns 
+ */
+export async function fetchStaticData() {
+    try {
+        const response = await fetch('./characterData.json');
+        const staticData = await response.json();
+        denormaliseData(staticData);
+        return staticData;
+    } catch (error) {
+        console.error('Error fetching JSON:', error);
+    }
+}
+
